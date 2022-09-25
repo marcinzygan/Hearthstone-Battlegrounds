d585435342282691a45124376a28ce15d0b55b61
marcinzygan <soul@o2.pl>
2022-09-25 09:26:38 +0200

    sorted out cards alphabetically , and set it as new state

--- src/App/Features/Cards/cardsSlice.js
+++ src/App/Features/Cards/cardsSlice.js
@@ -9,20 +9,26 @@ const cardsSlice = createSlice({
   initialState,
   reducers: {
     setData: (state, data) => {
+      //Filter out only cards with minion type
       const newState = data.payload
         .map((card, index) => {
           return card;
         })
         .filter((card) => card.img && card.type === "Minion");
       console.log(newState);
-
+      // Filter out duplicate cards
       const noDuplicates = newState.filter(
         (value, index, self) =>
           index === self.findIndex((card) => card.name === value.name)
       );
       console.log(noDuplicates);
-
-      state.cards = noDuplicates;
+      // Create copy of an array and sort it alphabetically
+      const sortedAlphabetically = [...noDuplicates].sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+      console.log(sortedAlphabetically);
+      // Set new state
+      state.cards = sortedAlphabetically;
     },
   },
 });
