da22833bb71ce0dd0ca8dd2b5fa21c2e23192b13
marcinzygan <soul@o2.pl>
2022-10-05 11:52:18 +0200

    added images and header component

---
+++ src/App/Features/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-background"></div>
+      <div className="header__menu-container"></div>
+    </div>
+  );
+};
+
+export default Header;
