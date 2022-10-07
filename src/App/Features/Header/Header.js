19cbeed1ee209575f395fd6a145ab6e9a3010a92
marcinzygan <soul@o2.pl>
2022-10-07 11:31:50 +0200

    added header styles and fonts

--- src/App/Features/Header/Header.js
+++ src/App/Features/Header/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import hsLogo from "../../../Images/hslogo.png";
 const Header = () => {
   return (
     <div className="header">
       <div className="header-background"></div>
-      <div className="header__menu-container"></div>
+      <div className="header__menu-container">
+        <button className="btn">Start</button>
+        <img src={hsLogo} alt="hearthstone logo" className="logo" />
+        <button className="btn">About</button>
+      </div>
     </div>
   );
 };
