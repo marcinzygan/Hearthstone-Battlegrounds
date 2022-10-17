22dd3d942ec0118ccd86ac8476931b61704f8e42
marcinzygan <soul@o2.pl>
2022-10-17 10:27:45 +0200

    added styles for welcome component

--- src/App/Features/Footer/Footer.js
+++ src/App/Features/Footer/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer">Designed and developed by Marcin Zygan</div>;
+  return (
+    <div className="footer">
+      <div className="footer__content">
+        Designed and developed by :
+        <span className="footer__span">
+          <a className="footer__link" href="https://marcin-zygan.com">
+            Marcin Zygan
+          </a>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
