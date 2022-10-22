e7bb909bdd1e5413ae5705a7d77295dba6ac6bc4
marcinzygan <soul@o2.pl>
2022-10-22 17:28:42 +0200

    fixed filter , and add to favourites bug

--- src/App/Features/Cards/cardsSlice.js
+++ src/App/Features/Cards/cardsSlice.js
@@ -36,12 +36,8 @@ const cardsSlice = createSlice({
       //Set original state for filterOptions
       state.originalCardsState = sortedAlphabetically;
     },
+    //FILTER DATA FEATURE
     filterData: (state, data) => {
-      // console.log(current(state.cards));
-      // state.originalState = [...state.cards];
-      // const newState = state.originalState;
-      // console.log(newState);
-
       if (data.payload === "All") {
         state.cards = state.originalCardsState;
       } else if (data.payload === "none") {
@@ -56,58 +52,55 @@ const cardsSlice = createSlice({
         state.cards = [...filteredCards];
       }
     },
+    //SET IMAGE URL  FEATURE
     setImg: (state, data) => {
       state.currentImg = data.payload;
     },
+    //ADD CARD TO FAVOURITES
     setFavourite: (state, data, index) => {
       const id = data.payload;
+      // Find the current card in state , copy its properties , and set isFav to true
       const newDisplayedCards = state.cards.map((card) => {
         if (card.cardId === id) {
           return { ...card, isFav: card.isFav === false ? true : false };
         }
         return { ...card };
       });
-
+      //Find current card in state.cards and add it to favouritesList
       const currentCard = state.cards.find((card) => card.cardId === id);
-
       state.favouritesList.push(currentCard);
-
-      // console.log(current(state.favouritesList));
-      // state.originalCardsState = [
-      //   ...state.originalCardsState.filter((card) => card.cardId != id),
-      //   ,
-      //   currentCard,
-      // ];
+      // Display updated state
       state.cards = newDisplayedCards;
 
+      //Find current card in originalState , copy its properties and change isFav to true
       const newOriginalCards = state.originalCardsState.map((card) => {
         if (card.cardId === id) {
           return { ...card, isFav: card.isFav === false ? true : false };
         }
         return { ...card };
       });
+      //Set new originalState
       state.originalCardsState = newOriginalCards;
     },
+    //REMOVE CARD FROM FAVOURITES
     removeFavourite: (state, data) => {
-      // const id = data.payload;
-
-      // const newFavList = state.favouritesList.filter(
-      //   (card) => card.cardId != id
-      // );
-      // console.log(newFavList);
       const id = data.payload;
+      // Find current card in cardsState , copy its properties and change isFav to false
       const newCards = state.cards.map((card) => {
         if (card.cardId === id) {
           return { ...card, isFav: card.isFav === false ? true : false };
         }
         return { ...card };
       });
+      //Set new original State and cardsState
 
-      state.originalCardsState = newCards;
       state.cards = newCards;
-      console.log(id);
-
-      // const currentCard = state.cards.find((card) => card.cardId === id);
+      const currentCard = state.cards.find((card) => card.cardId === id);
+      const newOriginalState = (state.originalCardsState = [
+        ...state.originalCardsState.filter((card) => card.cardId !== id),
+      ]);
+      state.originalCardsState = [...newOriginalState, currentCard];
+      //Filter out the current card from Favourites list
       state.favouritesList = state.favouritesList.filter(
         (card) => card.cardId !== id
       );
